420e7a3f2567e04e251a14e0685f93118b05f2a6
qyang18 <qyang18@hawk.iit.edu>
2016-02-25 15:46:57 -0600

    Update buffer_mgr.h

--- buffer_mgr.h
+++ buffer_mgr.h
@@ -1,43 +1,64 @@
-#ifndef STORAGE_MGR_H
-#define STORAGE_MGR_H
+#ifndef BUFFER_MANAGER_H
+#define BUFFER_MANAGER_H
 
+// Include return codes and methods for logging errors
 #include "dberror.h"
 
-/************************************************************
- *                    handle data structures                *
- ************************************************************/
-typedef struct SM_FileHandle {
-  char *fileName;
-  int totalNumPages;
-  int curPagePos;
-  void *mgmtInfo;
-} SM_FileHandle;
+// Include bool DT
+#include "dt.h"
 
-typedef char* SM_PageHandle;
+// Replacement Strategies
+typedef enum ReplacementStrategy {
+  RS_FIFO = 0,
+  RS_LRU = 1,
+  RS_CLOCK = 2,
+  RS_LFU = 3,
+  RS_LRU_K = 4
+} ReplacementStrategy;
 
-/************************************************************
- *                    interface                             *
- ************************************************************/
-/* manipulating page files */
-extern void initStorageManager (void);
-extern RC createPageFile (char *fileName);
-extern RC openPageFile (char *fileName, SM_FileHandle *fHandle);
-extern RC closePageFile (SM_FileHandle *fHandle);
-extern RC destroyPageFile (char *fileName);
+// Data Types and Structures
+typedef int PageNumber;
+#define NO_PAGE -1
 
-/* reading blocks from disc */
-extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern int getBlockPos (SM_FileHandle *fHandle);
-extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
+typedef struct BM_BufferPool {
+  char *pageFile;
+  int numPages;
+  ReplacementStrategy strategy;
+  void *mgmtData; // use this one to store the bookkeeping info your buffer 
+                  // manager needs for a buffer pool
+} BM_BufferPool;
 
-/* writing blocks to a page file */
-extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage);
-extern RC appendEmptyBlock (SM_FileHandle *fHandle);
-extern RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle);
+typedef struct BM_PageHandle {
+  PageNumber pageNum;
+  char *data;
+} BM_PageHandle;
+
+// convenience macros
+#define MAKE_POOL()					\
+  ((BM_BufferPool *) malloc (sizeof(BM_BufferPool)))
+
+#define MAKE_PAGE_HANDLE()				\
+  ((BM_PageHandle *) malloc (sizeof(BM_PageHandle)))
+
+// Buffer Manager Interface Pool Handling
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, 
+		  const int numPages, ReplacementStrategy strategy, 
+		  void *stratData);
+RC shutdownBufferPool(BM_BufferPool *const bm);
+RC forceFlushPool(BM_BufferPool *const bm);
+
+// Buffer Manager Interface Access Pages
+RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page);
+RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page);
+RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page);
+RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
+	    const PageNumber pageNum);
+
+// Statistics Interface
+PageNumber *getFrameContents (BM_BufferPool *const bm);
+bool *getDirtyFlags (BM_BufferPool *const bm);
+int *getFixCounts (BM_BufferPool *const bm);
+int getNumReadIO (BM_BufferPool *const bm);
+int getNumWriteIO (BM_BufferPool *const bm);
 
 #endif
